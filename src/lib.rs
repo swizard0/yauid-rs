bb6fb5d1bbd29ea6d3901f10ffbbe70c57858455
Alexey Voznyuk <me@swizard.info>
2015-11-26 03:10:01 +0300

    full yauid-rs crate impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,97 @@
 extern crate libc;
+extern crate yauid_sys;
 
+use std::{ptr, ffi};
+use yauid_sys::{yauid_init, yauid_destroy, yauid_get_key, yauid_set_node_id, yauid_get_error_text_by_code};
+
+pub struct Yauid {
+    _ffi_filepath_key: ffi::CString,
+    _ffi_filepath_node_id: Option<ffi::CString>,
+    ya: *mut yauid_sys::Yauid,
+}
+
+impl Drop for Yauid { fn drop(&mut self) { self.close(); } }
+
+impl Yauid {
+    pub fn new(filepath_key: &str, node_id: u64) -> Result<Yauid, String> {
+        let ffi_filepath_key = try!(ffi::CString::new(filepath_key.as_bytes()).map_err(|_| "invalid filepath_key".to_string()));
+        let ya = unsafe { yauid_init(ffi_filepath_key.as_ptr(), ptr::null()) };
+        if ya.is_null() {
+            Err("yauid_init failure".to_string())
+        } else {
+            let obj = Yauid {
+                _ffi_filepath_key: ffi_filepath_key,
+                _ffi_filepath_node_id: None,
+                ya: ya,
+            };
+            match unsafe { (*obj.ya).error } {
+                0 => {
+                    unsafe { yauid_set_node_id(obj.ya, node_id) };
+                    match unsafe { (*obj.ya).error } {
+                        0 => Ok(obj),
+                        status => Err(obj.status_to_string(status)),
+                    }
+                },
+                status => Err(obj.status_to_string(status)),
+            }
+        }
+    }
+
+    pub fn with_node_id(filepath_key: &str, filepath_node_id: &str) -> Result<Yauid, String> {
+        let ffi_filepath_key = try!(ffi::CString::new(filepath_key.as_bytes()).map_err(|_| "invalid filepath_key".to_string()));
+        let ffi_filepath_node_id = try!(ffi::CString::new(filepath_node_id.as_bytes()).map_err(|_| "invalid node_id".to_string()));
+        let ya = unsafe { yauid_init(ffi_filepath_key.as_ptr(), ffi_filepath_node_id.as_ptr()) };
+        if ya.is_null() {
+            Err("yauid_init failure".to_string())
+        } else {
+            let obj = Yauid {
+                _ffi_filepath_key: ffi_filepath_key,
+                _ffi_filepath_node_id: Some(ffi_filepath_node_id),
+                ya: ya,
+            };
+            match unsafe { (*obj.ya).error } {
+                0 => Ok(obj),
+                status => Err(obj.status_to_string(status)),
+            }
+        }
+    }
+
+    pub fn close(&mut self) {
+        if !self.ya.is_null() {
+            unsafe { yauid_destroy(self.ya) };
+        }
+    }
+
+    pub fn get_key(&self) -> Result<u64, String> {
+        let key = unsafe { yauid_get_key(self.ya) };
+        match unsafe { (*self.ya).error } {
+            0 => Ok(key as u64),
+            status => Err(self.status_to_string(status)),
+        }
+    }
+
+    fn status_to_string(&self, status: libc::c_int) -> String {
+        String::from_utf8_lossy(unsafe { ffi::CStr::from_ptr(yauid_get_error_text_by_code(status)).to_bytes() }).into_owned()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{Yauid};
+
+    #[test]
+    fn create_fail() {
+        match Yauid::new("/zz/qq/ww/yauid.tmp", 1) {
+            Ok(_) => panic!("Expected create error, but operation succeeded"),
+            Err(_) => ()
+        }
+    }
+
+    #[test]
+    fn create_get_drop() {
+        let y = Yauid::new("/tmp/yauid.tmp", 1).unwrap();
+        let id_a = y.get_key().unwrap();
+        let id_b = y.get_key().unwrap();
+        assert!(id_a < id_b);
+    }
+}
--- yauid-sys-rs/src/lib.rs
+++ yauid-sys-rs/src/lib.rs
@@ -1,2 +1,23 @@
 extern crate libc;
 
+use libc::{c_int, c_uint, c_ulong, c_char, c_void, uint64_t, useconds_t, FILE};
+
+#[repr(C)]
+pub struct Yauid {
+    i_lockfile: c_int,
+    c_lockfile: *const c_char,
+    h_lockfile: *mut FILE,
+    node_id: c_ulong,
+    try_count: c_uint,
+    sleep_usec: useconds_t,
+    pub error: c_int,
+    ext_value: *mut c_void,
+}
+
+extern {
+    pub fn yauid_init(filepath_key: *const c_char, filepath_node_id: *const c_char) -> *mut Yauid;
+    pub fn yauid_destroy(yauid: *mut Yauid);
+    pub fn yauid_get_key(yauid: *mut Yauid) -> uint64_t;
+    pub fn yauid_set_node_id(yauid: *mut Yauid, node_id: c_ulong);
+    pub fn yauid_get_error_text_by_code(error: c_int) -> *const c_char;
+}
